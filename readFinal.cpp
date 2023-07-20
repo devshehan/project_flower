5f26490b5e00b77ce763df85e19c6324ab2a8347
devshehan <shehaan.avishka00@gmail.com>
2023-07-20 07:49:38 +0530

    validateInput() function implemented. validated the inputs line by line and if it's valid input line return true

--- readFinal.cpp
+++ readFinal.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <regex>
+#include <unordered_set>
+
 using namespace std;
 
 bool isAlphaNumeric(string clientOrderID){
@@ -19,17 +21,67 @@ bool isAlphaNumeric(string clientOrderID){
 }
 
 // validate file input
-void validateInput(string clientOrderID, string instrument, string side, string price, string quantity){
+bool validateInput(string clientOrderID, string instrument, string side, string price, string quantity){
+
+    bool isClientOrderIDValid;
+    bool isInstrumentValid;
+    bool isSideValid;
+    bool isPriceValid;
+    bool isQuantityValid;
     
     // client order id
     if(clientOrderID.length() <=7 && isAlphaNumeric(clientOrderID)){
         // pass
-        cout << clientOrderID << " pass" << endl;
+        isClientOrderIDValid = true;
     }else{
-        cout << clientOrderID << "fail" << endl;
+        isClientOrderIDValid = false;
     }
 
-    // instruments
+    // instruments 
+    // search -> O(1)
+    unordered_set<string> instrumentSet = {"Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
+    // Capitalize first
+    transform (instrument.begin(), instrument.begin()+1, instrument.begin(),  ::toupper);
+    transform (instrument.begin()+1, instrument.end(),   instrument.begin()+1,::tolower);
+
+    //check instrument input is valid or not
+    if(instrumentSet.find(instrument) != instrumentSet.end()){
+        isInstrumentValid = true;
+    }else{
+        isInstrumentValid = false;
+    }
+
+    //Side
+    // convert string to integer (s to i)
+    int intSide = stoi(side);
+    if(intSide == 1 || intSide == 2){
+        isSideValid = true;
+    }else{
+        isSideValid = false;
+    }
+
+    //Price
+    double doublePrice = stod(price);
+    if(doublePrice > 0.0){
+        isPriceValid = true;
+    }else{
+        isPriceValid = false;
+    }
+
+    //Quantity
+    int intQuantity = stoi(quantity);
+    if( (intQuantity % 10 == 0) && intQuantity >= 10 && intQuantity <= 1000){
+        isQuantityValid = true;
+    }else{
+        isQuantityValid = false;
+    }
+
+    if(isClientOrderIDValid == true && isInstrumentValid == true && isSideValid == true && isPriceValid == true && isQuantityValid ==true){
+        return true;
+    }else{
+        return false;
+    }
+    
 }
 
 // read file variables
@@ -53,7 +105,9 @@ void readCsvFile(){
         getline(ip,Price,',');
         getline(ip,Quantity,'\n');
 
-        validateInput(Client_Order_ID, Instrument, Side, Price, Quantity);
+        bool isInputesAreValid = validateInput(Client_Order_ID, Instrument, Side, Price, Quantity);
+
+        cout << Client_Order_ID << " -> " << isInputesAreValid << endl;
     }
     ip.close();
 }
